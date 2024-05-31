27b67d82e35153ad9d6e57e413d2a06f1abeda13
Hongtauo <sunhongtao202103@163.com>
2024-06-01 00:17:56 +0800

    完成编码，准备提交

---
+++ FolderSpeed/FolderSpeed.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace FolderSpeed
+{
+    partial class FolderSpeed
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FolderSpeed));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.enteredAddress = new System.Windows.Forms.Label();
+            this.Enter = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.panel1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(362, 479);
+            this.splitContainer1.SplitterDistance = 65;
+            this.splitContainer1.TabIndex = 0;
+            this.splitContainer1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(362, 65);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("黑体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(338, 43);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "FolderSpeed";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.IsSplitterFixed = true;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.panel2);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.label3);
+            this.splitContainer2.Panel2.Controls.Add(this.label2);
+            this.splitContainer2.Size = new System.Drawing.Size(362, 410);
+            this.splitContainer2.SplitterDistance = 114;
+            this.splitContainer2.TabIndex = 0;
+            this.splitContainer2.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.enteredAddress);
+            this.panel2.Controls.Add(this.Enter);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(362, 114);
+            this.panel2.TabIndex = 0;
+            // 
+            // enteredAddress
+            // 
+            this.enteredAddress.Font = new System.Drawing.Font("黑体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.enteredAddress.Location = new System.Drawing.Point(16, 17);
+            this.enteredAddress.Name = "enteredAddress";
+            this.enteredAddress.Size = new System.Drawing.Size(332, 54);
+            this.enteredAddress.TabIndex = 1;
+            this.enteredAddress.Text = "路径:";
+            // 
+            // Enter
+            // 
+            this.Enter.Font = new System.Drawing.Font("黑体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Enter.Location = new System.Drawing.Point(109, 82);
+            this.Enter.Name = "Enter";
+            this.Enter.Size = new System.Drawing.Size(145, 31);
+            this.Enter.TabIndex = 0;
+            this.Enter.Text = "选择目标文件夹地址";
+            this.Enter.UseVisualStyleBackColor = true;
+            this.Enter.Click += new System.EventHandler(this.Enter_Click);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("黑体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(15, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(333, 224);
+            this.label2.TabIndex = 0;
+            this.label2.Text = resources.GetString("label2.Text");
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("黑体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(13, 236);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(333, 62);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "说明：\r\n本软件在”“上开源，禁止商业与恶意使用（包括但不限于售卖、修改并倒卖、植入恶意病毒等行为）。\r\n\r\n\r\n\r\n";
+            // 
+            // FolderSpeed
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(362, 479);
+            this.Controls.Add(this.splitContainer1);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FolderSpeed";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FolderSpeed";
+            this.Load += new System.EventHandler(this.FolderSpeed_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label enteredAddress;
+        private System.Windows.Forms.Button Enter;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ FolderSpeed/FolderSpeed.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace FolderSpeed
+{
+    public partial class FolderSpeed : Form
+    {
+        public string targetPath = string.Empty;
+        static public string configFile = "Config.xml";
+        public bool flag = false;
+        public string configFilePath = Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, configFile);
+        public FolderSpeed()
+        {
+            InitializeComponent();
+            this.MaximumSize = this.Size;
+            this.MinimumSize = this.Size;
+
+            // 检查是否存在配置文件，不存在的话先创建
+            CreateConfigFile();
+            // 从本地的配置文件中加载targetPath
+            LoadtargetPath();
+            // 打开tagrgetPath对应的文件夹,如果路径为空，则弹出界面让用户选择路径，然后写入到配置文件中
+            OpenFolder();
+
+        }
+        private void CreateConfigFile()
+        {
+            //如果该配置文件不存在的话，则在exe运行目录下创建一个Config.xml文件
+            // 检查文件是否存在
+            if (File.Exists(configFilePath))
+            {
+                return;
+            }
+            // 创建一个新的XML文档
+            XmlDocument doc = new XmlDocument();
+            // 创建XML声明
+            XmlDeclaration dec = doc.CreateXmlDeclaration("1.0", "UTF-8", null);
+            doc.AppendChild(dec);
+            // 创建根节点
+            XmlElement root = doc.CreateElement("Configuration");
+            doc.AppendChild(root);
+            // 创建Path节点
+            XmlElement path = doc.CreateElement("Path");
+            path.InnerText = ""; 
+            root.AppendChild(path);
+            // 保存文档
+            doc.Save(configFilePath);
+        }
+
+        public void LoadtargetPath()
+        {
+            // 检查文件是否存在
+            if (File.Exists(configFilePath))
+            {
+                // 创建XML文档对象
+                XmlDocument doc = new XmlDocument();
+                // 加载XML文件
+                doc.Load(configFilePath);
+                // 获取Path节点的值
+                XmlNode node = doc.SelectSingleNode("//Path");
+                if (node != null)
+                {
+                    targetPath = node.InnerText;
+                }
+                else
+                {
+                    Console.WriteLine("未找到Path");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Config.xml文件不存在");
+            }
+        }
+
+        public void OpenFolder()
+        {
+            if (Directory.Exists(targetPath))
+            {
+                System.Diagnostics.Process.Start(targetPath);
+                this.flag = true; // 如果能正常打开，那就将标志设置为true，然后Load的时候就能通过这个判断是显示窗口还是退出程序
+            }          
+        }
+
+        public void UpdataConfigFile(string item)
+        {
+            // 调用一下，以防配置文件不存在的情况下报错
+            CreateConfigFile();
+            // 创建XML文档对象
+            XmlDocument doc = new XmlDocument();
+            // 加载XML文件
+            doc.Load(configFilePath);
+            // 获取Path节点
+            XmlNode node = doc.SelectSingleNode("//Path");
+            if (node != null)
+            {
+                // 更新
+                node.InnerText = item;
+                // 保存
+                doc.Save(configFilePath);
+                enteredAddress.Text = "路径:"+item; 
+            }
+            else
+            {
+                Console.WriteLine("未找到Path节点");
+            }
+        }
+
+        private void FolderSpeed_Load(object sender, EventArgs e)
+        {
+            // 如果配置文件为空或者路径不存在，则不退出窗体，显示界面让用户进行修改目标文件夹
+            if (!this.flag)
+            { 
+                return;
+            }           
+            // 退出主程序
+            this.Close();
+        }
+
+        private void Enter_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog dialog = new FolderBrowserDialog())
+            {
+                // 显示对话框
+                DialogResult result = dialog.ShowDialog();
+
+                // 点击确定按钮
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(dialog.SelectedPath))
+                {
+                    // 获取选择的文件夹地址
+                    string selectedFolderPath = dialog.SelectedPath;
+                    // 更新路径
+                    UpdataConfigFile(selectedFolderPath);
+                }
+            }
+        }
+    }
+}
